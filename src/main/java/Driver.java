ed4e63997585fa867b0fb312e20816c9513c1190
Fathian Alfiana Rahman <alfianafr@student.telkomuniversity.ac.id>
2026-06-11 07:48:13 +0700

    trigger sonarcloude

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,21 +1,20 @@
-import src.main.java.Counter;
 
-public class Driver { //ini driver 
+public class Driver { // ini driver
 
 	public static void main(String[] args) {
-		
+
 		Counter counter = new Counter();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 		counter.increment();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 		counter.decrement();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 	}
 
 }
